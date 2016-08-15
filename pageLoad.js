de2c1be796663d1990dd49788a8ab01f2873609e
Jammy Laird <jammylaird@yahoo.com>
2016-08-15 07:09:12 -0500

    working on page js to load the page

---
+++ pageLoad.js
@@ -0,0 +1,21 @@
+var Xplode = (function(call){
+  var fireworks=[]
+  var desruction =[]
+  call.loadPage = (categories, types, products){
+    for(var i in products){
+      if (products[i].type <=2){
+        fireworks.push(products[i])
+      }
+      else {
+        destruction.push(products[i])
+
+      }
+    }
+$("#fireworks").click("fireworks")
+$("#destruction"). click("destruction")
+
+function fireworks(){
+  $()
+  }
+}
+})
